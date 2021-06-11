e7eeebef987e38e071083296302ce420f5d705b2
BezymiannyiOlexii <71652686+BezymiannyiOlexii@users.noreply.github.com>
2021-06-11 10:59:05 +0300

    Update test_file.py

--- test_file.py
+++ test_file.py
@@ -108,31 +108,6 @@ def disk():
     return render_template('disk.html', data=data)
 
 
-@app.route('/disk', methods=['GET', 'POST'])
-def disk():
-    database_files = os.listdir('database/')
-    database_sizes = list()
-    for file in database_files:
-        file_size = os.path.getsize('database/'+file)
-        if file_size > 10**9:
-            file_size /= 1024**3
-            file_weight = 'Gb'
-            size = '%.2f'%file_size+' {}'.format(file_weight)
-        elif file_size > 10**6:
-            file_size /= 1024**2
-            file_weight = 'MB'
-            size = '%.2f' % file_size + ' {}'.format(file_weight)
-        else:
-            file_size /= 1024
-            file_weight = 'kB'
-            size = '%.2f' % file_size + ' {}'.format(file_weight)
-
-        database_sizes.append(size)
-    print(database_files, database_sizes)
-    data = zip(database_files, database_sizes)
-    return render_template('disk.html', data=data)
-
-
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=4446, debug=True)
 
